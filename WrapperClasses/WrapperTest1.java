f67d8cdfccb7366a9fa54800feaad6b50ec9d439
sumitranjanDroider <desidroider@gmail.com>
2024-02-19 08:57:23 +0530

    Wrapper Practice Strted

---
+++ WrapperClasses/WrapperTest1.java
@@ -0,0 +1,18 @@
+/*
+Primitive Data Type with there Wrapper Classes
+boolean -Boolean
+byte - Byte
+char - Character
+float -Float
+int - Integer
+long - Long
+short - Short
+double - Double
+*/
+class WrapperTest1{
+
+public static void main(String[] args){
+  System.out.println("Revision Of Java WrapperClassesTopic");
+   
+}
+}
